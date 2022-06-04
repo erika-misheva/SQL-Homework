bcd77249f1869a2a578d22e2f90f62597a54222d
erika-misheva <misevaerika@gmail.com>
2022-06-04 18:28:29 +0200

    Commiting the second homework.

---
+++ SecondHomework/SecondHomework.sql
@@ -0,0 +1,31 @@
+USE SEDCACADEMYDB;
+
+SELECT * FROM Student
+WHERE FirstName = 'Antonio';
+
+SELECT * FROM Student
+WHERE DateOfBirth > '1999-01-01';
+
+SELECT * FROM Student
+WHERE LastName like 'J%' AND EnrolledDate between '1998-01-01' and '1998-02-01'; 
+
+SELECT * FROM Student
+ORDER BY FirstName ASC;
+
+SELECT DISTINCT LastName, FirstName from Student;
+
+SELECT Course.[Name] AS 'Course Name', AchievementType.[Name] AS 'Achievement Name' 
+FROM Course
+CROSS JOIN AchievementType
+WHERE AchievementType.[Name] = 'Ispit';
+
+SELECT *
+FROM Teacher
+LEFT JOIN Grade
+ON Teacher.ID= Grade.TeacherID
+Where Grade.ID is Null
+ORDER BY FirstName;
+
+
+
+
