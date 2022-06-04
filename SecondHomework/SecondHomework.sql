3e57d8ab7ead3e0ef841ff2a8a279f4eec38aa47
erika-misheva <misevaerika@gmail.com>
2022-06-04 19:01:56 +0200

    Commiting the second homework.

--- SecondHomework/SecondHomework.sql
+++ SecondHomework/SecondHomework.sql
@@ -12,12 +12,16 @@ WHERE LastName like 'J%' AND EnrolledDate between '1998-01-01' and '1998-02-01';
 SELECT * FROM Student
 ORDER BY FirstName ASC;
 
-SELECT DISTINCT LastName, FirstName from Student;
+SELECT LastName
+FROM Student
+UNION
+SELECT LastName
+FROM Teacher;
 
 SELECT Course.[Name] AS 'Course Name', AchievementType.[Name] AS 'Achievement Name' 
 FROM Course
 CROSS JOIN AchievementType
-WHERE AchievementType.[Name] = 'Ispit';
+WHERE AchievementType.[Name] = 'Ispit'; 
 
 SELECT *
 FROM Teacher
@@ -25,6 +29,11 @@ LEFT JOIN Grade
 ON Teacher.ID= Grade.TeacherID
 Where Grade.ID is Null
 ORDER BY FirstName;
+
+Select * FROM AchievementType;
+
+
+
 
 
 
